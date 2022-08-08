eae0921134b08c2759009f7015b680596306238e
SemirBaldzhiev <semir.baldzhiev@gmail.com>
2022-08-08 13:01:32 +0300

    Add exam solutions

--- Week4/Day1/exam/task-1.c
+++ Week4/Day1/exam/task-1.c
@@ -12,7 +12,7 @@ Node* insertAfter(Node *cll, int skipCount, int newElem){
     new_node->data = newElem;
     
     Node *curr = cll;
-    for(int i = 0; i<skipCount;i++){
+    for(int i = 0; i<skipCount-1;i++){
       curr = curr->next;
     }
    new_node->next = curr->next;
@@ -21,15 +21,16 @@ Node* insertAfter(Node *cll, int skipCount, int newElem){
 
 void print(Node *cll){
     Node *curr = cll;
-
-    do{
+    while(curr->next != cll){
         printf("%d ", curr->data);
         curr = curr->next;
-    }while(curr != cll);
+        //printf("here");
+    }
+    printf("%d", curr->data);
     putchar('\n');
 }
 
-void push_back(Node *head, int data){
+void push_back(Node **head, int data){
     Node *new_node = malloc(sizeof(Node));
 
     if (!new_node) {
@@ -38,22 +39,22 @@ void push_back(Node *head, int data){
     } 
 
     new_node->data = data;
-    new_node->next = head;
+    new_node->next = *head;
     
-    if(!(head)){
-        head = new_node;
-        head->next = new_node;
+    if(!(*head)){
+        *head = new_node;
+        (*head)->next = new_node;
         return;
     }
 
-    Node *last_node = head;
+    Node *last_node = *head;
 
     do{
         last_node = last_node->next;
-    }while(last_node->next != head);
+    }while(last_node->next != *head);
 
     if(!last_node){
-        last_node = head;
+        last_node = *head;
     }
 
     // while (last_node->next){
@@ -64,29 +65,41 @@ void push_back(Node *head, int data){
 
 void free_cll(Node *head){
     Node *current;
-    while (head){
+    // while (head){
+    //     current = head;
+    //     head = head->next;
+    //     free(current);
+    // }
+    Node *h = head;
+    do{
         current = head;
         head = head->next;
         free(current);
-    }
+    }while(current->next != h);   
 }
 
 int main() {
 
-    Node *head = malloc(sizeof(Node));
-    head->data = 1;
-    head->next = head;
-    int n;
-    scanf("%d", &n);
+    Node *head = NULL;
+    push_back(&head, 1);
+    push_back(&head, 2);
+    push_back(&head, 3);
 
-    for (int i = 0; i < n; i++){
-        int d;
-        scanf("%d", &d);
-        push_back(head, d);
-    }
-    printf("here");
+    //printf("%d ", head->data);
+    //printf("%p ", head->next);
+    insertAfter(head, 1, 5);
+
+    // int n;
+    // scanf("%d", &n);
+
+    // for (int i = 0; i < n; i++){
+    //     int d;
+    //     scanf("%d", &d);
+    //     push_back(head, d);
+    // }
+    // printf("here");
     print(head);
-    printf("here");
-    //free_cll(head);
+    // printf("here");
+    free_cll(head);
     return 0;
 }
